9eca74052194c8a54c7a99bd1bd99428850a0773
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-06-06 19:44:18 -0700

    modify index to load page contents on button click change main-content style to class, not id change blacksword to woff2 instead of otf

--- src/index.js
+++ src/index.js
@@ -2,3 +2,23 @@
 
 import "./css/josh-comeau-reset.css";
 import "./css/styles.css";
+
+import { home as homeContent } from "./js/home.js";
+import { menu as menuContent } from "./js/menu.js";
+import { about as aboutContent } from "./js/about.js";
+
+const content = document.getElementById("content");
+const btnHome = document.getElementById("nav-home");
+const btnMenu = document.getElementById("nav-menu");
+const btnAbout = document.getElementById("nav-about");
+
+btnHome.addEventListener("click", () => changeContent(homeContent));
+btnMenu.addEventListener("click", () => changeContent(menuContent));
+btnAbout.addEventListener("click", () => changeContent(aboutContent));
+
+function changeContent(elementsToAdd) {
+    content.innerHTML = "";
+    elementsToAdd.forEach(e => content.appendChild(e));
+}
+
+changeContent(homeContent);
